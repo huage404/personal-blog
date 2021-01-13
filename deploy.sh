6ef0978db75b0a27b4c1dbc9e2b4274b47a470c7
去年夏天 <6523611+zhenhua4207@user.noreply.gitee.com>
2021-01-13 23:50:55 +0800

    [修改]修改 shell 脚本

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,23 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# 删除之前创建的 public
+rm -rf public
+
 # 生成静态文件
 npm run build
 
 # 进入生成的文件夹
-# cd public
+cd public
 
-# git init
-git branch -M main
+git init
 git add .
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
-git remote add origin origin https://github.com/huage404/my_bolg.git
+git remote add origin git@github.com:huage404/my_bolgs_website.git
 
-git push -u origin main
+git push -u origin master --force
 
 cd ..
 
