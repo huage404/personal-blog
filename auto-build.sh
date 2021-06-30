573feb3caa694e4bc1b50f8eee412a498f97083d
huage <1962408497@qq.com>
2021-06-30 13:26:07 +0800

    deploy

---
+++ auto-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# 确保脚本抛出遇到的错误
+set -e
+
+cd /home/huage/code/personal-blog/
+
+git pull
+
+yarn build
--- deploy.sh
+++ deploy.sh
@@ -3,29 +3,11 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-# 删除之前创建的 public
-rm -rf dist
-
-# 生成静态文件
-npm run build
-
-# 进入生成的文件夹
-cd dist
-
-# 添加域名配置文件
-echo www.xiazhenhua.cn >> CNAME
-
 git init
 git add .
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
-git remote add origin git@github.com:huage404/my_blogs_website.git
-git remote add origin2 git@gitee.com:zhenhua4207/my_blogs_website.git
+git remote add origin  https://gitee.com/zhenhua4207/personal-blog.git
 
-git push -u origin master --force
-git push -u origin2 master --force
+git push -u origin main --force
 
-cd ..
-
-#tcb hosting:deploy public -e blog-9g8lgnuke4603ff9
